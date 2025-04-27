3cab775804251ed7ee3a6fc245a583de36c4556c
siroj05 <rojudin123@gmail.com>
2025-04-27 23:04:26 +0700

    prepare statement

--- golang local.session.sql
+++ golang local.session.sql
@@ -1,28 +1,3 @@
+SELECT * FROM user;
 
--- SELECT * FROM customer;
-
--- DELETE FROM customer;
-
--- ALTER TABLE customer
---   ADD COLUMN email VARCHAR(100),
---   ADD COLUMN balance INT DEFAULT 0,
---   ADD COLUMN rating DOUBLE DEFAULT 0.0,
---   ADD COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---   ADD COLUMN birth_date DATE,
---   ADD COLUMN marriage BOOLEAN DEFAULT false;
-
--- DESC customer;
-
--- INSERT INTO customer(id, name, email, balance, rating, birth_date, marriage)
--- VALUES ('eko', 'EKO', 'eko@gmail.com', 100000, 5.0, '1999-9-9', true);
-
--- INSERT INTO customer(id, name, email, balance, rating, birth_date, marriage)
--- VALUES ('budi', 'Budi', 'budi@gmail.com', 100000, 5.0, '1999-9-9', true);
-
--- INSERT INTO customer(id, name, email, balance, rating, birth_date, marriage)
--- VALUES ('budi', 'Budi', null, 750000, 88.5, '1999-9-9', false);
-
-UPDATE customer
-  SET email = NULL,
-  birth_date = NULL,
-  WHERE id = 'eko'
+INSERT INTO user(username, password) VALUES ('admin', 'admin');
