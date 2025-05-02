c3ef7f2ae309c10dfb70fb83b38d9e0ca85e4318
siroj05 <rojudin123@gmail.com>
2025-05-02 21:20:56 +0700

    go embed

---
+++ golang-embed/embed_test.go
@@ -0,0 +1,30 @@
+package golang_embed
+
+import (
+	_ "embed"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"testing"
+)
+
+//go:embed version.txt
+var version string
+
+//go:embed version.txt
+var version2 string
+
+func TestString(t *testing.T) {
+	fmt.Println(version)
+	fmt.Println(version2)
+}
+
+//go:embed logo.png
+var logo []byte
+
+func TestByte(t *testing.T) {
+	err := ioutil.WriteFile("logo_new.png", logo, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+}
